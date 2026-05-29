72ebfc38efa334a4f5668455d17cf0f949409bf5
SYD Constructores <quieromicasa@sydconstructores.com.mx>
2026-05-29 00:49:14 -0600

    UI: Ocultar versionBadge global y bump v1.1.13

--- js/app.js
+++ js/app.js
@@ -52,7 +52,7 @@ window.addEventListener('appinstalled', () => {
 
 
 // SERVICE WORKER & UPDATES
-const APP_VERSION = 'v1.1.12';
+const APP_VERSION = 'v1.1.13';
 
 // Auto-fill all version placeholders
 function fillVersionBadges() {
